c36ee0e3a9adfad97ccc6421bd632d0f71ce17a4
Stéphanie <145790456+StyMey@users.noreply.github.com>
2023-10-17 17:11:18 +0200

    Update2 index.php

--- index.php
+++ index.php
@@ -5,40 +5,36 @@ require_once '_connec.php';
 $pdo = new \PDO(DSN, USER, PASS);
 
 if(!empty($_POST)) {
-    $friend = $_POST;
-    $friend = array_map('trim', $friend);
-    $friend = array_map('htmlentities', $friend);
+    $friends = $_POST;
+    $friends = array_map('trim', $friends);
+    $friends = array_map('htmlentities', $friends);
 
     $errors = [];
 
-    if(empty($friend['firstname']) || !isset($friend['firstname'])) {
+    if(empty($friends['firstname']) || !isset($friends['firstname'])) {
         $errors[] = 'le prénom est obligatoire';
     }
-    if(empty($friend['lastname']) || !isset($friend['lastname'])) {
+    if(empty($friends['lastname']) || !isset($friends['lastname'])) {
         $errors[] = 'le nom est obligatoire';
     }
 
     if(empty($errors)) {
-        header('Location: success.php');
-    }
-    $query = 'INSERT INTO friend (firstname, lastname) VALUES (:firstname, :lastname)';
-    $statement = $pdo->prepare($query);
+        
+        $query = 'INSERT INTO friend (firstname, lastname) VALUES (:firstname, :lastname)';
+        $statement = $pdo->prepare($query);
 
-    $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
-    $statement->bindValue(':lastname', $lastname, \PDO::PARAM_STR);
-    $statement->execute();
+        $statement->bindValue(':firstname', $friends['firstname'], \PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $friends['lastname'], \PDO::PARAM_STR);
+        $statement->execute();
+
+        header('Location: index.php');
+    }
 }
 
 $query = "SELECT * FROM friend";
 $statement = $pdo->query($query);
-$friend = $statement->fetchAll();
+$friendsArray = $statement->fetchAll();
 
-(PDO::FETCH_ASSOC);
-$friendsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($friendsArray as $friend) {
-    echo $friend['firstname'] . ' ' . $friend['lastname'];
-}
 
 ?>
 
@@ -51,14 +47,17 @@ foreach($friendsArray as $friend) {
 </head>
 <body>
     <main>
-        <form>
-            <select>
+        <ul>
+            <?php foreach($friendsArray as $friend): ?>
+            <li><?=$friend['firstname'] . ' ' . $friend['lastname']?></li>
+            <?php endforeach;?>
+        </ul>
+        <form method="post">
                 <option value = "select">Sélectionner</option>
                 <option value = "friend1">Monica</option>
                 <option value = "friend2">Phoebe</option>
                 <option value = "friend3">Joey</option>
                 <option value = "friend4">Ross</option>
-            </select>
             <div>
                 <label for="firstname">Prénom</label>
                 <input type="text" id="firstname" name="firstname">
